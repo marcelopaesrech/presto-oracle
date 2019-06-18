50ee064cc0351215553b216373cd09ea48bd238d
Tweea <tweea@263.net>
2019-06-18 11:25:00 +0800

    根据 Oracle JDBC JavaDoc 文档增加配置参数

--- src/main/java/io/prestosql/plugin/oracle/OracleClient.java
+++ src/main/java/io/prestosql/plugin/oracle/OracleClient.java
@@ -13,21 +13,18 @@
  */
 package io.prestosql.plugin.oracle;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import io.prestosql.plugin.jdbc.BaseJdbcClient;
 import io.prestosql.plugin.jdbc.BaseJdbcConfig;
 import io.prestosql.plugin.jdbc.ColumnMapping;
+import io.prestosql.plugin.jdbc.ConnectionFactory;
 import io.prestosql.plugin.jdbc.DriverConnectionFactory;
-import io.prestosql.plugin.jdbc.JdbcColumnHandle;
 import io.prestosql.plugin.jdbc.JdbcIdentity;
-import io.prestosql.plugin.jdbc.JdbcTableHandle;
 import io.prestosql.plugin.jdbc.JdbcTypeHandle;
 import io.prestosql.plugin.jdbc.WriteMapping;
 import io.prestosql.spi.PrestoException;
 import io.prestosql.spi.connector.ConnectorSession;
 import io.prestosql.spi.connector.SchemaTableName;
-import io.prestosql.spi.connector.TableNotFoundException;
 import io.prestosql.spi.type.BigintType;
 import io.prestosql.spi.type.BooleanType;
 import io.prestosql.spi.type.CharType;
@@ -42,7 +39,6 @@ import io.prestosql.spi.type.Type;
 import io.prestosql.spi.type.VarbinaryType;
 import io.prestosql.spi.type.VarcharType;
 import oracle.jdbc.OracleDriver;
-import oracle.jdbc.driver.OracleConnection;
 
 import javax.inject.Inject;
 
@@ -51,13 +47,14 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.UUID;
 import java.util.function.BiFunction;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static io.prestosql.plugin.jdbc.DriverConnectionFactory.basicConnectionProperties;
 import static io.prestosql.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
 import static io.prestosql.plugin.jdbc.StandardColumnMappings.bigintWriteFunction;
 import static io.prestosql.plugin.jdbc.StandardColumnMappings.booleanWriteFunction;
@@ -80,7 +77,6 @@ import static io.prestosql.spi.StandardErrorCode.NOT_SUPPORTED;
 import static io.prestosql.spi.type.VarcharType.createUnboundedVarcharType;
 import static io.prestosql.spi.type.Varchars.isVarcharType;
 import static java.lang.String.format;
-import static java.sql.DatabaseMetaData.columnNoNulls;
 import static java.util.Locale.ENGLISH;
 
 public class OracleClient
@@ -89,7 +85,49 @@ public class OracleClient
     @Inject
     public OracleClient(BaseJdbcConfig config, OracleConfig oracleConfig)
     {
-        super(config, "\"", new DriverConnectionFactory(new OracleDriver(), config));
+        super(config, "\"", connectionFactory(config, oracleConfig));
+    }
+
+    private static ConnectionFactory connectionFactory(BaseJdbcConfig config, OracleConfig oracleConfig)
+    {
+        Properties connectionProperties = basicConnectionProperties(config);
+        if (!isNullOrEmpty(oracleConfig.getDatabase())) {
+            connectionProperties.setProperty("database", oracleConfig.getDatabase());
+        }
+        if (!isNullOrEmpty(oracleConfig.getInternalLogon())) {
+            connectionProperties.setProperty("internal_logon", oracleConfig.getInternalLogon());
+        }
+        if (oracleConfig.getDefaultRowPrefetch() != null) {
+            connectionProperties.setProperty("defaultRowPrefetch", oracleConfig.getDefaultRowPrefetch().toString());
+        }
+        if (oracleConfig.getRemarksReporting() != null) {
+            connectionProperties.setProperty("remarksReporting", oracleConfig.getRemarksReporting().toString());
+        }
+        if (oracleConfig.getDefaultBatchValue() != null) {
+            connectionProperties.setProperty("defaultBatchValue", oracleConfig.getDefaultBatchValue().toString());
+        }
+        if (oracleConfig.getIncludeSynonyms() != null) {
+            connectionProperties.setProperty("includeSynonyms", oracleConfig.getIncludeSynonyms().toString());
+        }
+        if (oracleConfig.getProcessEscapes() != null) {
+            connectionProperties.setProperty("processEscapes", oracleConfig.getProcessEscapes().toString());
+        }
+        if (oracleConfig.getDefaultNChar() != null) {
+            connectionProperties.setProperty("defaultNChar", oracleConfig.getDefaultNChar().toString());
+        }
+        if (oracleConfig.getUseFetchSizeWithLongColumn() != null) {
+            connectionProperties.setProperty("useFetchSizeWithLongColumn", oracleConfig.getUseFetchSizeWithLongColumn().toString());
+        }
+        if (oracleConfig.getSetFloatAndDoubleUseBinary() != null) {
+            connectionProperties.setProperty("SetFloatAndDoubleUseBinary", oracleConfig.getSetFloatAndDoubleUseBinary().toString());
+        }
+
+        return new DriverConnectionFactory(
+                new OracleDriver(),
+                config.getConnectionUrl(),
+                Optional.ofNullable(config.getUserCredentialName()),
+                Optional.ofNullable(config.getPasswordCredentialName()),
+                connectionProperties);
     }
 
     @Override
@@ -102,42 +140,6 @@ public class OracleClient
                 schemaNames.add(schemaName);
             }
             return schemaNames.build();
-        }
-        catch (SQLException e) {
-            throw new PrestoException(JDBC_ERROR, e);
-        }
-    }
-
-    @Override
-    public List<JdbcColumnHandle> getColumns(ConnectorSession session, JdbcTableHandle tableHandle)
-    {
-        try (Connection connection = connectionFactory.openConnection(JdbcIdentity.from(session))) {
-            // If the table is mapped to another user you will need to get the synonym to that table
-            // So, in this case, is mandatory to use setIncludeSynonyms
-            ((OracleConnection) connection).setIncludeSynonyms(true);
-            try (ResultSet resultSet = getColumns(tableHandle, connection.getMetaData())) {
-                List<JdbcColumnHandle> columns = new ArrayList<>();
-                while (resultSet.next()) {
-                    JdbcTypeHandle typeHandle = new JdbcTypeHandle(
-                            resultSet.getInt("DATA_TYPE"),
-                            Optional.ofNullable(resultSet.getString("TYPE_NAME")),
-                            resultSet.getInt("COLUMN_SIZE"),
-                            resultSet.getInt("DECIMAL_DIGITS"),
-                            Optional.empty());
-                    Optional<ColumnMapping> columnMapping = toPrestoType(session, connection, typeHandle);
-                    // skip unsupported column types
-                    if (columnMapping.isPresent()) {
-                        String columnName = resultSet.getString("COLUMN_NAME");
-                        boolean nullable = (resultSet.getInt("NULLABLE") != columnNoNulls);
-                        columns.add(new JdbcColumnHandle(columnName, typeHandle, columnMapping.get().getType(), nullable));
-                    }
-                }
-                if (columns.isEmpty()) {
-                    // In rare cases (e.g. PostgreSQL) a table might have no columns.
-                    throw new TableNotFoundException(tableHandle.getSchemaTableName());
-                }
-                return ImmutableList.copyOf(columns);
-            }
         }
         catch (SQLException e) {
             throw new PrestoException(JDBC_ERROR, e);
--- src/main/java/io/prestosql/plugin/oracle/OracleConfig.java
+++ src/main/java/io/prestosql/plugin/oracle/OracleConfig.java
@@ -13,6 +13,147 @@
  */
 package io.prestosql.plugin.oracle;
 
+import io.airlift.configuration.Config;
+
 public class OracleConfig
 {
+    private String database;
+
+    private String internalLogon;
+
+    private Integer defaultRowPrefetch;
+
+    private Boolean remarksReporting;
+
+    private Integer defaultBatchValue;
+
+    private Boolean includeSynonyms;
+
+    private Boolean processEscapes;
+
+    private Boolean defaultNChar;
+
+    private Boolean useFetchSizeWithLongColumn;
+
+    private Boolean setFloatAndDoubleUseBinary;
+
+    public String getDatabase()
+    {
+        return database;
+    }
+
+    @Config("oracle.database")
+    public OracleConfig setDatabase(String database)
+    {
+        this.database = database;
+        return this;
+    }
+
+    public String getInternalLogon()
+    {
+        return internalLogon;
+    }
+
+    @Config("oracle.internal-logon")
+    public OracleConfig setInternalLogon(String internalLogon)
+    {
+        this.internalLogon = internalLogon;
+        return this;
+    }
+
+    public Integer getDefaultRowPrefetch()
+    {
+        return defaultRowPrefetch;
+    }
+
+    @Config("oracle.default-row-prefetch")
+    public OracleConfig setDefaultRowPrefetch(Integer defaultRowPrefetch)
+    {
+        this.defaultRowPrefetch = defaultRowPrefetch;
+        return this;
+    }
+
+    public Boolean getRemarksReporting()
+    {
+        return remarksReporting;
+    }
+
+    @Config("oracle.remarks-reporting")
+    public OracleConfig setRemarksReporting(Boolean remarksReporting)
+    {
+        this.remarksReporting = remarksReporting;
+        return this;
+    }
+
+    public Integer getDefaultBatchValue()
+    {
+        return defaultBatchValue;
+    }
+
+    @Config("oracle.default-batch-value")
+    public OracleConfig setDefaultBatchValue(Integer defaultBatchValue)
+    {
+        this.defaultBatchValue = defaultBatchValue;
+        return this;
+    }
+
+    public Boolean getIncludeSynonyms()
+    {
+        return includeSynonyms;
+    }
+
+    @Config("oracle.include-synonyms")
+    public OracleConfig setIncludeSynonyms(Boolean includeSynonyms)
+    {
+        this.includeSynonyms = includeSynonyms;
+        return this;
+    }
+
+    public Boolean getProcessEscapes()
+    {
+        return processEscapes;
+    }
+
+    @Config("oracle.process-escapes")
+    public OracleConfig setProcessEscapes(Boolean processEscapes)
+    {
+        this.processEscapes = processEscapes;
+        return this;
+    }
+
+    public Boolean getDefaultNChar()
+    {
+        return defaultNChar;
+    }
+
+    @Config("oracle.default-nchar")
+    public OracleConfig setDefaultNChar(Boolean defaultNChar)
+    {
+        this.defaultNChar = defaultNChar;
+        return this;
+    }
+
+    public Boolean getUseFetchSizeWithLongColumn()
+    {
+        return useFetchSizeWithLongColumn;
+    }
+
+    @Config("oracle.use-fetch-size-with-long-column")
+    public OracleConfig setUseFetchSizeWithLongColumn(Boolean useFetchSizeWithLongColumn)
+    {
+        this.useFetchSizeWithLongColumn = useFetchSizeWithLongColumn;
+        return this;
+    }
+
+    public Boolean getSetFloatAndDoubleUseBinary()
+    {
+        return setFloatAndDoubleUseBinary;
+    }
+
+    @Config("oracle.set-float-and-double-use-binary")
+    public OracleConfig setSetFloatAndDoubleUseBinary(Boolean setFloatAndDoubleUseBinary)
+    {
+        this.setFloatAndDoubleUseBinary = setFloatAndDoubleUseBinary;
+        return this;
+    }
 }
